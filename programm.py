81fafe14d377322e1d635bc5f6114923d3686fe1
Sonu Kumar Singh <hiersonusingh@gmail.com>
2023-03-10 17:22:49 +0530

    programm.py

---
+++ programm.py
@@ -0,0 +1,9 @@
+# print("Hello World")
+
+num = int(input("Enter the number: "))
+if(num>0):
+    print(f'{num} is positive')
+elif(num<0):
+    print(f'{num} is negative')
+else:
+    print(f'{num} is zero')
